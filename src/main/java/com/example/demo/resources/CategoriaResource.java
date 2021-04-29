23e1265fad60a0d24defad9c7f46a349f6c41860
Davi Araujo <moreiradaavi@gmail.com>
2021-04-29 19:55:09 -0300

    Criando a Primeira Classe Dominio

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,15 @@ public class CategoriaResource {
 
 
 @RequestMapping(method = RequestMethod.GET)
-public String listar() {
-	return "Rest funcionando!!";
+public List<Categoria> listar() {
+	List<Categoria> lista = new ArrayList<>();
+	
+	Categoria cat1 = new Categoria(1, "Informática");
+	Categoria cat2 = new Categoria(2, "Escritorio");
+	
+	lista.add(cat1);
+	lista.add(cat2);
+	return lista;
 	
 }
 
